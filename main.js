0922a57aec4b734d294ab8fec5141fa9b1577802
jokak286-droid <jokak286@gmail.com>
2026-09-15 18:37:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,8 +8,13 @@ alert("hello!");
 
 var myButton = document.querySelector("button");
 var myHeading = document.querySelector("h1");
+var myText = document.querySelector("p");
+var myList = document.querySelector("li");
 
 function setUserName() {
+  var myText = document.querySelector("p");
+var myList = document.querySelector("li");
+
   var myName = prompt("Please enter your name.");
   localStorage.setItem("name", myName);
   myHeading.textContent = "Hello, " + myName;
@@ -17,14 +22,25 @@ function setUserName() {
   myList.textContent = "pepperoni L";
 }
 
+var myText = document.querySelector("p");
+var myList = document.querySelector("li");
 
 
 if (!localStorage.getItem("name")) {
+  var myText = document.querySelector("p");
+var myList = document.querySelector("li");
+
   setUserName();
 } else {
   var storedName = localStorage.getItem("name");
+  var myText = document.querySelector("p");
+var myList = document.querySelector("li");
+
   myHeading.textContent = "Hello, " + storedName;
 }
 myButton.onclick = function () {
   setUserName();
 };
+var myText = document.querySelector("p");
+var myList = document.querySelector("li");
+
